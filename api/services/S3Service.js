2bea824ea059328e0aa055ae82e2ba64ef17ea6e
dirisu.jesse <dirisujesse@gmail.com>
2018-06-17 00:02:28 +0100

    change

--- api/services/S3Service.js
+++ api/services/S3Service.js
@@ -9,6 +9,8 @@ module.exports = {
 			  Bucket: 'reportapp-bucket',
 			  Key: title,
 			  Body: bs4Str,
+			  ContentEncoding: 'base64',
+			  ContentType:  'image/jpeg',
 			}
 			s3.upload(params, (err, data) => {
 			  if (err) {
