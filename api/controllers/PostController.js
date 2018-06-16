16bc45e972474c3d6e9e597e875c79bd6f171013
dirisu.jesse <dirisujesse@gmail.com>
2018-06-16 23:34:58 +0100

    change

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -7,45 +7,50 @@
 
 module.exports = {
 	createPost: function(req, res) {
-		// S3Service.uploadToS3(img);
 		GeolocationService.getCoordLoc(req.param('lat'), req.param('long')).then(function(loc) {
-			Post.create({
-				upvotes: 0,
-				downvotes: 0,
-				title: req.param('title'),
-				body: req.param('body'),
-				author: req.param('author'),
-				time:  new Date().toUTCString().replace("GMT", "").trim(),
-				loc: loc,
-				lat: req.param('lat') || 0,
-				long: req.param('long') || 0
-			})
-			.exec(function (err, post){
-			  if (err) { 
-			  	return res.negotiate(err); 
-			  }
-			  if (!post) {
-			  	return res.negotiate(err); 
-			  }
-			  if (post) {
-			  	Post
-				.findOne({
-					id: post.id,
+			S3Service.uploadToS3(req.param('img'))
+			.then(function(url) {
+				Post.create({
+					upvotes: 0,
+					downvotes: 0,
+					title: req.param('title'),
+					body: req.param('body'),
+					author: req.param('author'),
+					time:  new Date().toUTCString().replace("GMT", "").trim(),
+					loc: loc,
+					lat: req.param('lat') || 0,
+					long: req.param('long') || 0,
+					image: url
 				})
-				.populate('author')
 				.exec(function (err, post){
 				  if (err) { 
 				  	return res.negotiate(err); 
 				  }
 				  if (!post) {
-				  	return res.negotiate(); 
+				  	return res.negotiate(err); 
 				  }
 				  if (post) {
-				  	return res.json(post);
+				  	Post
+					.findOne({
+						id: post.id,
+					})
+					.populate('author')
+					.exec(function (err, post){
+					  if (err) { 
+					  	return res.negotiate(err); 
+					  }
+					  if (!post) {
+					  	return res.negotiate(); 
+					  }
+					  if (post) {
+					  	return res.json(post);
+					  }
+					});
 				  }
 				});
-			  }
-			});
+			}).catch(function(err) {
+				return res.negotiate(err);
+			})
 		})
 	},
 	getPost: function(req, res) {
--- api/services/S3Service.js
+++ api/services/S3Service.js
@@ -3,21 +3,20 @@ AWS.config.update({ accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: pro
 var s3 = new AWS.S3()
 
 module.exports = {
-	uploadToS3: function(bs4Str) {
-		var params = {
-		  Bucket: 'reportapp-bucket',
-		  Key: `avatar`,
-		  Body: bs4Str,
-		  ACL: 'public-read',
-		  ContentEncoding: 'base64',
-		}
-
-		s3.upload(params, (err, data) => {
-		  if (err) { return console.log(err) }
-		  
-		  // Continue if no error
-		  // Save data.Location in your database
-		  console.dir(data);
-		});
+	uploadToS3: function(bs4Str, title) {
+		return new Promise(function(res, rej) {
+			var params = {
+			  Bucket: 'reportapp-bucket',
+			  Key: title,
+			  Body: bs4Str,
+			}
+			s3.upload(params, (err, data) => {
+			  if (err) {
+			  	rej(err);
+			  } else {
+			  	res(data.Location)
+			  }
+			});
+		})
 	}
 }
