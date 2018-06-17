aeb82f5b43daeba6c181cbd24ce727886c0dce12
dirisu.jesse <dirisujesse@gmail.com>
2018-06-17 01:17:03 +0100

    change

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -8,47 +8,47 @@
 module.exports = {
 	createPost: function(req, res) {
 		GeolocationService.getCoordLoc(req.param('lat'), req.param('long')).then(function(loc) {
-			S3Service.uploadToS3(req.param('img'), `${req.param('title')}_${new Date().valueOf()}`)
-			.then(function(url) {
-				Post.create({
-					upvotes: 0,
-					downvotes: 0,
-					title: req.param('title'),
-					body: req.param('body'),
-					author: req.param('author'),
-					time:  new Date().toUTCString().replace("GMT", "").trim(),
-					loc: loc,
-					lat: req.param('lat') || 0,
-					long: req.param('long') || 0,
-					image: url
-				})
-				.exec(function (err, post){
-				  if (err) { 
-				  	return res.negotiate(err); 
-				  }
-				  if (!post) {
-				  	return res.negotiate(err); 
-				  }
-				  if (post) {
-				  	Post
-					.findOne({
-						id: post.id,
-					})
-					.populate('author')
-					.exec(function (err, post){
-					  if (err) { 
-					  	return res.negotiate(err); 
-					  }
-					  if (!post) {
-					  	return res.negotiate(); 
-					  }
-					  if (post) {
-					  	return res.json(post);
-					  }
-					});
-				  }
-				});
-			}).catch(function(err) {
+			// S3Service.uploadToS3(req.param('img'), `${req.param('title')}_${new Date().valueOf()}`)
+			// .then(function(url) {
+			// 	Post.create({
+			// 		upvotes: 0,
+			// 		downvotes: 0,
+			// 		title: req.param('title'),
+			// 		body: req.param('body'),
+			// 		author: req.param('author'),
+			// 		time:  new Date().toUTCString().replace("GMT", "").trim(),
+			// 		loc: loc,
+			// 		lat: req.param('lat') || 0,
+			// 		long: req.param('long') || 0,
+			// 		image: url
+			// 	})
+			// 	.exec(function (err, post){
+			// 	  if (err) { 
+			// 	  	return res.negotiate(err); 
+			// 	  }
+			// 	  if (!post) {
+			// 	  	return res.negotiate(err); 
+			// 	  }
+			// 	  if (post) {
+			// 	  	Post
+			// 		.findOne({
+			// 			id: post.id,
+			// 		})
+			// 		.populate('author')
+			// 		.exec(function (err, post){
+			// 		  if (err) { 
+			// 		  	return res.negotiate(err); 
+			// 		  }
+			// 		  if (!post) {
+			// 		  	return res.negotiate(); 
+			// 		  }
+			// 		  if (post) {
+			// 		  	return res.json(post);
+			// 		  }
+			// 		});
+			// 	  }
+			// 	});
+			// }).catch(function(err) {
 				Post.create({
 					upvotes: 0,
 					downvotes: 0,
@@ -86,7 +86,7 @@ module.exports = {
 					});
 				  }
 				});
-			})
+			// })
 		})
 	},
 	getPost: function(req, res) {
